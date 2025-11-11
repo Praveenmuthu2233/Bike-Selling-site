1204754c88a93dd5c7331f59b918e0d7270bd971
Praveenmuthu2233 <praveenmuthu9500@gmail.com>
2025-11-11 23:08:12 +0530

    force update

--- my-node-app/server.js
+++ my-node-app/server.js
@@ -62,7 +62,8 @@ function startServer() {
   const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }
-initializeDatabase();
+
+//initializeDatabase();
 startServer();
 
 function initializeDatabase() {
