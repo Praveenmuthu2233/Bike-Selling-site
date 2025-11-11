8675528f2846c103b79b53a0188cf60ed9cb25e1
Praveenmuthu2233 <praveenmuthu9500@gmail.com>
2025-11-11 22:29:40 +0530

    force update

--- my-node-app/server.js
+++ my-node-app/server.js
@@ -23,14 +23,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // if (!fs.existsSync('./uploads')) fs.mkdirSync('./uploads');
 
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, "uploads/");
-//   },
-//   filename: function (req, file, cb) {
-//     cb(null, Date.now() + "-" + file.originalname);
-//   }
-// });const upload = multer({ storage: storage });
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage });
 
 // const upload = multer();
 // const storage = multer.diskStorage({
