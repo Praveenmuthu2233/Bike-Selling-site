775b2acdcf22febf8eadadf8be338c6a973fa971
Praveenmuthu2233 <praveenmuthu9500@gmail.com>
2025-11-11 22:49:25 +0530

    force update

--- my-node-app/server.js
+++ my-node-app/server.js
@@ -15,7 +15,13 @@ const JWT_SECRET = process.env.JWT_SECRET;
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 app.use(bodyParser.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -56,17 +62,8 @@ function startServer() {
   const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }
-
-db.getConnection((err, connection) => {
-  if (err) {
-    console.error("MySQL Connection Failed:", err.message);
-    process.exit(1);
-  }
-  console.log("MySQL Connected Successfully");
-  connection.release();
-  initializeDatabase();
-  startServer();
-});
+initializeDatabase();
+startServer();
 
 function initializeDatabase() {
   const queries = [
