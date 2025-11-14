59e03146c5f05480f4c1b939699ef033fd1c3fc2
Praveenmuthu2233 <praveenmuthu9500@gmail.com>
2025-11-14 19:38:40 +0530

    Update code

--- my-node-app/server.js
+++ my-node-app/server.js
@@ -410,3 +410,6 @@ app.get('/checkMobile/:mobile', (req, res) => {
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+app.get('/bike-details', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'bike-details.html'));
+});
